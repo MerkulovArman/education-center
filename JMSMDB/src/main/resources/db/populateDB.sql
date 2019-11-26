402464de947d45d5d32eaf27b5dc1566ed45924e
MerkulovArman <merkylov.arman@gmail.com>
2019-11-26 14:32:34 +0400

    code refactoring

--- JMSMDB/src/main/resources/db/populateDB.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM users;
-ALTER SEQUENCE user_seq RESTART WITH 1;
-
-INSERT INTO users (name, email, surname) VALUES
-  ('Ivan', 'ivanov@yandex.ru', 'Ivanov'),
-  ('Max', 'max@gmail.com', 'Petrov');
